b8cdf61abf03e9a32caea501eebd66a78e1a46df
Celil Ozturk <celil.ozturk@windowslive.com>
2023-11-22 11:01:50 +0300

    example

--- App.tsx
+++ App.tsx
@@ -18,11 +18,6 @@ let soyad="AYDIN";
 let mesaj=`Merhaba, benim adim ${ad} soyadim ${soyad}`;
 console.log(mesaj);
 
-let marka="Ford";
-let model="Mustang";
-let renk="Gri";
-
-let mesaj2=`Benim ${marka} marka ${model} model ${renk} bir arabam var`;
 
 function App(): JSX.Element {
 
@@ -30,8 +25,6 @@ function App(): JSX.Element {
   return (
     <SafeAreaView >
       <View>
-        <Text style={styles.mesaj}>{mesaj}</Text>
-        <Text style={styles.mesaj2}>{mesaj2}</Text>
         <Text style={styles.metin}>Merhaba Dunya</Text>
       </View>
     </SafeAreaView>
