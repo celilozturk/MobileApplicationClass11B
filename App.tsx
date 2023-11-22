6b0faaa082beb6656b8ed1bde72f17a16aca417d
Celil Ozturk <celil.ozturk@windowslive.com>
2023-11-22 11:39:49 +0300

    example layout, backtick and styles

--- App.tsx
+++ App.tsx
@@ -6,44 +6,66 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-let ad="Ege";
-let soyad="AYDIN";
+import { SafeAreaView, StyleSheet, Text, View, } from 'react-native';
+let ad = "Emir";
+let soyad = "ORHAN";
+let sinif = "11B";
+let ders = "Mobile Uygulamalar";
+let mesaj = `Hosgeldin ${ad}`;
+let year = 2023;
 
-let mesaj=`Merhaba, benim adim ${ad} soyadim ${soyad}`;
-console.log(mesaj);
 
 
 function App(): JSX.Element {
-
-
   return (
-    <SafeAreaView >
-      <View>
-        <Text style={styles.metin}>Merhaba Dunya</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box1}>
+        <Text style={styles.mesaj}>{mesaj}</Text>
+      </View>
+      <View style={styles.box2}>
+          <Text style={styles.textStyle}>Ad: {ad}</Text>
+          <Text style={styles.textStyle}>Soyad: {soyad}</Text>
+          <Text style={styles.textStyle}>Sinif: {sinif}</Text>
+          <Text style={styles.textStyle}>Ders: {ders}</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text style={styles.textStyle}> {year}</Text>
       </View>
     </SafeAreaView>
   );
 }
 
-const styles=StyleSheet.create({
-  mesaj:{
-    color:'red',
-    backgroundColor:'gray',
+const styles = StyleSheet.create({
+
+  mesaj: {
+    textAlign: 'center',
+    color: 'red',
+    backgroundColor: 'black'
   },
-  mesaj2:{
-    color:'green',
-    backgroundColor:'yellow',
+  container: {
+    flex: 1, padding: 10
   },
-  metin:{
-    textAlign:'center',
-    color:'red',
-    backgroundColor:'Black'
+  box1: {
+    flex: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center'
+  },
+  box2: {
+    flex: 4,
+    backgroundColor: 'orange',
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  textStyle: {
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 
 })
